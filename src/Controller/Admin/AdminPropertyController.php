7067263e7d1d0a7b5bf7b150bb6744497b0005ac
inoubli <aymen.inoubli@gmail.com>
2018-12-15 20:30:31 +0100

    Administration

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminPropertyController extends  AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/admin", name = "admin.property.index")
+     * @return \Symfony\Component\HttpFoundation\Response;
+     */
+    public function index()
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
+
+    }
+
+    /**
+     * @Route("/admin/{id}", name = "admin.property.edit")
+     */
+    public function edit(Property $property)
+    {
+        return $this->render("admin/property/edit.html.twig", compact("property"));
+    }
+
+
+
+
+
+
+
+
+
+
+}
